abab9e43618c0d186de8061234d3a3abb2f7e243
isozaki29 <keisuke.isozaki.2021@gmail.com>
2022-11-19 21:01:21 +0900

    #2対応

--- src/main/java/com/example/kadai9/controller/MovieController.java
+++ src/main/java/com/example/kadai9/controller/MovieController.java
@@ -3,30 +3,32 @@ package com.example.kadai9.controller;
 import com.example.kadai9.entity.Movie;
 import com.example.kadai9.mapper.MovieMapper;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/movies")
 public class MovieController {
-
+    
     private final MovieMapper movieMapper;
 
     public MovieController(MovieMapper movieMapper) {
         this.movieMapper = movieMapper;
     }
 
-    //全件取得
-    @GetMapping("/movies")
+    ///全件取得
+    @GetMapping
     public List<Movie> getMovie() {
         List<Movie> movie = movieMapper.findAll();
         return movie;
     }
 
     //公開年から映画情報を取得
-    @GetMapping("/movies/{publishedYear}")
-    public List<Movie> getMovie(@PathVariable("publishedYear") int publishedYear) {
+    @GetMapping("/publishedYear")
+    public List<Movie> getMovie(@RequestParam("publishedYear") int publishedYear) {
         List<Movie> movie = movieMapper.findByPublishedYear(publishedYear);
         return movie;
     }
--- src/main/java/com/example/kadai9/entity/Movie.java
+++ src/main/java/com/example/kadai9/entity/Movie.java
@@ -1,26 +1,14 @@
 package com.example.kadai9.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class Movie {
+
     private int id;
     private String name;
     private int publishedYear;
-
-    public Movie(int id, String name, int publishedYear) {
-        this.id = id;
-        this.name = name;
-        this.publishedYear = publishedYear;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getPublishedYear() {
-        return publishedYear;
-    }
 
 }
